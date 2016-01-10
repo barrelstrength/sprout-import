4c924d91bfc7830fc9ee599bbc02338d2c215b51
Ben Parizek <ben@barrelstrengthdesign.com>
2016-01-09 22:36:28 -0800

    Adds SproutMigrate_SeedService Class

---
+++ services/SproutMigrate_SeedService.php
@@ -0,0 +1,79 @@
+<?php
+namespace Craft;
+
+class SproutMigrate_SeedService extends BaseApplicationComponent
+{
+	public $seed = false;
+
+	public function trackSeed($itemId = null, $importerClass = null)
+	{
+		if (!$itemId OR !$importerClass)
+		{
+			return false;
+		}
+
+		$record                = new SproutMigrate_SeedRecord;
+		$record->itemId        = $itemId;
+		$record->importerClass = $importerClass;
+
+		$record->save();
+	}
+
+	public function getAllSeeds()
+	{
+		$seeds = craft()->db->createCommand()
+										->select('itemId, importerClass')
+										->from('sproutmigrate_seeds')
+										->queryAll();
+
+		return $seeds;
+	}
+
+	public function weed()
+	{
+		$results = craft()->db->createCommand()
+			->select('id, itemId, importerClass')
+			->from('sproutmigrate_seeds')
+			->queryAll();
+
+		$transaction = craft()->db->getCurrentTransaction() === null ? craft()->db->beginTransaction() : null;
+
+		foreach ($results as $row)
+		{
+			try
+			{
+				// @todo - improve how we handle this setting...
+				// we're just adding it to the array here...
+				$stringId = explode('_',$row['importerClass']);
+				$row['@id'] = implode('.', array_map('strtolower', $stringId));
+
+				$importer = sproutMigrate()->getImporter($row);
+				$importer->deleteById($row['itemId']);
+
+				$this->deleteSeedById($row['id']);
+
+			}
+			catch (\Exception $e)
+			{
+				SproutMigratePlugin::log($e->getMessage());
+			}
+		}
+
+		if ($transaction && $transaction->active)
+		{
+			$transaction->commit();
+		}
+
+		return true;
+
+	}
+
+	public function deleteSeedById($id)
+	{
+		return craft()->db->createCommand()->delete(
+				'sproutmigrate_seeds',
+				'id=:id',
+				array(':id'=>$id)
+		);
+	}
+}
