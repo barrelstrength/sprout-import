348f862198e34c9c287cf46fef7f0fae531cab54
Dale Ramirez <dalefirstpage@gmail.com>
2018-05-17 18:51:42 +0800

    - Refactor Order import that acceps transactions and address that links to the current customer

--- src/importers/elements/Order.php
+++ src/importers/elements/Order.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace barrelstrength\sproutimport\importers\elements;
+
 use barrelstrength\sproutbase\app\import\base\ElementImporter;
 use barrelstrength\sproutimport\SproutImport;
 use craft\commerce\base\Gateway;
@@ -14,24 +16,24 @@ use craft\commerce\records\Transaction;
 
 class Order extends ElementImporter
 {
-	/** @var string Session key for storing the cart number */
-	protected $cookieCartId = 'commerce_cookie';
+    /** @var string Session key for storing the cart number */
+    protected $cookieCartId = 'commerce_cookie';
 
-	/**
-	 * @return string
-	 */
-	public function getName()
-	{
-		return Craft::t('sprout-import', 'Commerce Orders');
-	}
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return Craft::t('sprout-import', 'Commerce Orders');
+    }
 
-	/**
-	 * @return mixed
-	 */
-	public function getModelName()
-	{
-		return OrderElement::class;
-	}
+    /**
+     * @return mixed
+     */
+    public function getModelName()
+    {
+        return OrderElement::class;
+    }
 
     /**
      * @param       $model
@@ -42,97 +44,102 @@ class Order extends ElementImporter
      * @throws \craft\errors\ElementNotFoundException
      * @throws \yii\base\Exception
      */
-	public function setModel($model, array $settings = [])
-	{
-		$this->model = parent::setModel($model, $settings);
+    public function setModel($model, array $settings = [])
+    {
+        $this->model = parent::setModel($model, $settings);
 
         $this->model = Plugin::getInstance()->getCarts()->getCart();
-
-		if ($number = $settings['attributes']['number']) {
-		    $orderCart = Plugin::getInstance()->getOrders()->getOrderByNumber($number);
-		    if ($orderCart) {
+        $number = $settings['attributes']['number'] ?? null;
+        if ($number) {
+            $orderCart = Plugin::getInstance()->getOrders()->getOrderByNumber($number);
+            if ($orderCart) {
                 $this->model = $orderCart;
             }
         }
 
-		$this->model->setAttributes($settings['attributes'], false);
+        $this->model->setAttributes($settings['attributes'], false);
 
-		if (is_string($settings['attributes']['customerId'])) {
-		    $user = Craft::$app->users->getUserByUsernameOrEmail($settings['attributes']['customerId']);
+        $customerEmail = $settings['attributes']['customerId'] ?? null;
 
-		    if ($user) {
-                $customer = Plugin::getInstance()->getCustomers()->getCustomerByUserId((int) $user->id);
+        if (is_string($customerEmail)) {
+            $user = Craft::$app->users->getUserByUsernameOrEmail($customerEmail);
+
+            if ($user) {
+                $customer = Plugin::getInstance()->getCustomers()->getCustomerByUserId((int)$user->id);
 
                 if ($customer) {
                     $this->model->customerId = $customer->id;
-                } else  {
+                } else {
                     $customer = new Customer();
 
                     if ($user) {
                         $customer->userId = $user->id;
                     }
-
-                    Plugin::getInstance()->getCustomers()->saveCustomer($customer);
-
-                    if ($address = $settings['addresses']['billingAddress']) {
-                        $billingAddress = new Address();
-
-                        $billingAddress->firstName = $address['firstName'];
-                        $billingAddress->lastName  = $address['lastName'];
-                        $countryCode = $address['countryCode'];
-
-                        $countryObj = Plugin::getInstance()->getCountries()->getCountryByIso($countryCode);
-                        $billingAddress->countryId = $countryObj ? $countryObj->id : null;
-
-                        if ($billingAddress->countryId) {
-                            $stateObj= Plugin::getInstance()
-                                ->getStates()
-                                ->getStateByAbbreviation($billingAddress->countryId, $address['state']);
-
-                            $stateId = $stateObj ? $stateObj->id : null;
-
-                            if ($stateId) {
-                                $billingAddress->stateId = $stateId;
-                            } else {
-                                $billingAddress->stateName = $address['state'];
-                            }
-
-                            $billingAddress->address1 = $address['address1'];
-                            $billingAddress->city = $address['city'];
-                        }
-
-                        $billingAddress->zipCode = $address['zipCode'];
-
-                        Plugin::getInstance()->getCustomers()->saveAddress($billingAddress, $customer);
-
-                        $customer->primaryBillingAddressId = $billingAddress->id;
-                        $customer->primaryShippingAddressId = $billingAddress->id;
-
-                        Plugin::getInstance()->getCustomers()->saveCustomer($customer);
-                    }
-
-
-                    if ($customer->id) {
-                        $this->model->customerId = $customer->id;
-                    }
-                }
-
-                if ($this->model->customer) {
-                    $billingAddress = $this->model->customer->getPrimaryBillingAddress();
-
-                    $shippingAddress = $this->model->customer->getPrimaryShippingAddress();
-                    $this->model->setBillingAddress($billingAddress);
-                    $this->model->setShippingAddress($shippingAddress);
                 }
             }
+        } else {
+            $customer = Plugin::getInstance()->getCustomers()->getCustomerById($customerEmail);
         }
 
-		if ($settings['lineItems']) {
-		    foreach ($settings['lineItems'] as $item) {
+        Plugin::getInstance()->getCustomers()->saveCustomer($customer);
+        $address = $settings['addresses']['billingAddress'] ?? null;
+        if ($address) {
+            $billingAddress = new Address();
 
-		        $purchasableId = $item['purchasableId'] ?? null;
+            $billingAddress->firstName = $address['firstName'];
+            $billingAddress->lastName = $address['lastName'];
+            $countryCode = $address['countryCode'];
 
-		        if ($sku = $item['sku']) {
+            $countryObj = Plugin::getInstance()->getCountries()->getCountryByIso($countryCode);
+            $billingAddress->countryId = $countryObj ? $countryObj->id : null;
+
+            if ($billingAddress->countryId) {
+                $stateObj = Plugin::getInstance()
+                    ->getStates()
+                    ->getStateByAbbreviation($billingAddress->countryId, $address['state']);
+
+                $stateId = $stateObj ? $stateObj->id : null;
+
+                if ($stateId) {
+                    $billingAddress->stateId = $stateId;
+                } else {
+                    $billingAddress->stateName = $address['state'];
+                }
+
+                $billingAddress->address1 = $address['address1'];
+                $billingAddress->city = $address['city'];
+            }
+
+            $billingAddress->zipCode = $address['zipCode'];
+
+            Plugin::getInstance()->getCustomers()->saveAddress($billingAddress, $customer);
+
+            $customer->primaryBillingAddressId = $billingAddress->id;
+            $customer->primaryShippingAddressId = $billingAddress->id;
+
+            Plugin::getInstance()->getCustomers()->saveCustomer($customer);
+        }
+
+        if ($customer->id) {
+            $this->model->customerId = $customer->id;
+        }
+
+        if ($this->model->customer) {
+            $billingAddress = $this->model->customer->getPrimaryBillingAddress();
+
+            $shippingAddress = $this->model->customer->getPrimaryShippingAddress();
+            $this->model->setBillingAddress($billingAddress);
+            $this->model->setShippingAddress($shippingAddress);
+        }
+
+        if ($settings['lineItems']) {
+            foreach ($settings['lineItems'] as $item) {
+
+                $purchasableId = $item['purchasableId'] ?? null;
+
+                $sku = $item['sku'] ?? null;
+
+                if ($sku) {
                     $purchasable = Purchasable::find()->where(['sku' => $sku])->one();
 
                     if ($purchasable) {
@@ -146,21 +153,21 @@ class Order extends ElementImporter
             }
         }
 
-		$this->model->isCompleted = $settings['attributes']['isCompleted'] ?? 1;
+        $this->model->isCompleted = $settings['attributes']['isCompleted'] ?? 1;
 
         $orderStatus = Plugin::getInstance()->getOrderStatuses()->getDefaultOrderStatusForOrder($this->model);
-		if ($orderStatus) {
+        if ($orderStatus) {
             $this->model->orderStatusId = $orderStatus->id;
         }
 
-		$this->model->paymentCurrency = Plugin::getInstance()->getPaymentCurrencies()->getPrimaryPaymentCurrencyIso();
-	}
+        $this->model->paymentCurrency = Plugin::getInstance()->getPaymentCurrencies()->getPrimaryPaymentCurrencyIso();
+    }
 
     /**
      * @return bool|void
      * @throws \Throwable
      */
-	public function save()
+    public function save()
     {
         parent::save();
 
@@ -209,7 +216,6 @@ class Order extends ElementImporter
                             $transactionRecord->save();
                         }
                     }
-
                 } catch (PaymentException $exception) {
                     $utilities->addError('invalid-payment', $exception->getMessage());
                 }
@@ -226,36 +232,35 @@ class Order extends ElementImporter
      * @return bool
      * @throws \Throwable
      */
-	public function deleteById($id)
-	{
-		return Craft::$app->elements->deleteElementById($id);
-	}
+    public function deleteById($id)
+    {
+        return Craft::$app->elements->deleteElementById($id);
+    }
 
-	public function getImporterDataKeys()
-	{
-		return array('lineItems', 'payments', 'transactions', 'addresses');
-	}
+    public function getImporterDataKeys()
+    {
+        return ['lineItems', 'payments', 'transactions', 'addresses'];
+    }
 
-	/**
-	 * This is code is from Commerce_CartService copied it because it is private
-	 *
-	 * @return mixed|string
-	 */
-	private function getRandomCartNumber()
-	{
-		$number = md5(uniqid(mt_rand(), true));
+    /**
+     * This is code is from Commerce_CartService copied it because it is private
+     *
+     * @return mixed|string
+     */
+    private function getRandomCartNumber()
+    {
+        $number = md5(uniqid(mt_rand(), true));
 
-		$order = Plugin::getInstance()->getOrders()->getOrderByNumber($number);
-		// Make sure not duplicate number
-		if ($order)
-		{
-			return $this->getRandomCartNumber();
-		}
+        $order = Plugin::getInstance()->getOrders()->getOrderByNumber($number);
+        // Make sure not duplicate number
+        if ($order) {
+            return $this->getRandomCartNumber();
+        }
 
-		return $number;
-	}
+        return $number;
+    }
 
-	public function getFieldLayoutId($model)
+    public function getFieldLayoutId($model)
     {
         // TODO: Implement getFieldLayoutId() method.
     }
