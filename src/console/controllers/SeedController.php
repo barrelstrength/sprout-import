e218406979bfa4f504472dab36fe442e62ad0027
Dale Ramirez <dalefirstpage@gmail.com>
2018-09-18 19:31:57 +0800

    - Tested solution by passing json string and decoded it in the method

--- src/console/controllers/SeedController.php
+++ src/console/controllers/SeedController.php
@@ -2,58 +2,21 @@
 namespace barrelstrength\sproutimport\console\controllers;
 
 use yii\console\Controller;
-use barrelstrength\sproutimport\models\Json;
-use barrelstrength\sproutimport\queue\jobs\Import;
-use barrelstrength\sproutimport\models\Seed;
-use sproutimport\enums\ImportType;
-use Craft;
-use yii\console\ExitCode;
 
 class SeedController extends Controller
 {
-    public $filePath;
+    public $settings;
 
     public function options($actionID)
     {
-        return ['filePath'];
+        return ['settings'];
     }
 
     public function actionIndex()
     {
-        if (!file_exists($this->filePath)) {
-            $message = Craft::t("sprout-import", "File path does not exist.");
-            $this->stdout($message);
-
-            return ExitCode::DATAERR;
-        }
-
-        $fileContent = file_get_contents($this->filePath);
-
-        if ($fileContent) {
-            $seedModel = new Seed();
-            $seedModel->seedType = ImportType::Console;
-            $seedModel->enabled = 1;
-
-            $importData = new Json();
-            $importData->setJson($fileContent);
-
-            // Make sure we have JSON
-            if ($importData->hasErrors()) {
-                $errorMessage = $importData->getFirstError('json');
-
-                $this->stdout($errorMessage);
-
-                return ExitCode::DATAERR;
-            }
-
-            $fileImportJob = new Import();
-            $fileImportJob->seedAttributes = $seedModel->getAttributes();
-            $fileImportJob->importData = $importData->json;
-
-            Craft::$app->queue->push(new Import([
-                'importData' => $fileImportJob->importData,
-                'seedAttributes' => $fileImportJob->seedAttributes
-            ]));
-        }
+        $options = $this->getPassedOptionValues();
+        \Craft::dd($this->settings, 10, false);
+        \Craft::dd(json_decode($this->settings), 10, false);
+        \Craft::dd($options, 10, false);
     }
 }
