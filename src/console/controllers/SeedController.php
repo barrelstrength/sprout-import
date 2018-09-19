8975bc13c3f0b72af0a5d8881e427a1fed11d3c7
Dale Ramirez <dalefirstpage@gmail.com>
2018-09-19 18:53:01 +0800

    Created Seed console class that generate seeds based on the pass class namespace, quantity and the settings path for the class seeds

--- src/console/controllers/SeedController.php
+++ src/console/controllers/SeedController.php
@@ -1,22 +1,58 @@
 <?php
 namespace barrelstrength\sproutimport\console\controllers;
 
+use barrelstrength\sproutimport\SproutImport;
+use craft\helpers\DateTimeHelper;
 use yii\console\Controller;
+use craft\helpers\Json;
+use Craft;
+use yii\console\ExitCode;
+use barrelstrength\sproutimport\models\jobs\SeedJob;
+use sproutimport\enums\ImportType;
 
 class SeedController extends Controller
 {
-    public $settings;
+    public $content;
+    public $settingPath;
+    public $quantity;
 
     public function options($actionID)
     {
-        return ['settings'];
+        return ['content', 'quantity', 'settingPath'];
     }
 
     public function actionIndex()
     {
-        $options = $this->getPassedOptionValues();
-        \Craft::dd($this->settings, 10, false);
-        \Craft::dd(json_decode($this->settings), 10, false);
-        \Craft::dd($options, 10, false);
+        if (!file_exists($this->settingPath)) {
+            $message = Craft::t("sprout-import", "File path does not exist.");
+            $this->stdout($message);
+
+            return ExitCode::DATAERR;
+        }
+
+        $jsonSetting = file_get_contents($this->settingPath);
+
+        $setting = Json::decode($jsonSetting);
+
+        $weedMessage = Craft::t('sprout-import', '{elementType} Element(s)');
+
+        $details = Craft::t('sprout-import', $weedMessage, [
+            'elementType' => $this->content
+        ]);
+
+        $seedJob = new SeedJob();
+        $seedJob->elementType = $this->content;
+        $seedJob->quantity = !empty($this->quantity) ? $this->quantity : 11;
+        $seedJob->settings = $setting;
+        $seedJob->seedType = ImportType::Seed;
+        $seedJob->details = $details;
+        $seedJob->dateCreated = DateTimeHelper::currentUTCDateTime();
+
+        $seedJobErrors = null;
+
+        if (SproutImport::$app->seed->generateSeeds($seedJob)) {
+            $message = Craft::t("sprout-import", $this->content . " seed in queue.");
+            $this->stdout($message. PHP_EOL);
+        }
     }
 }
