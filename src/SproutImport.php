6b4a7846f937922ea9030b8e84e69e5d5b4d84ec
Ben Parizek <ben@barrelstrengthdesign.com>
2018-10-27 15:46:06 -0400

    Update nav

--- src/SproutImport.php
+++ src/SproutImport.php
@@ -104,10 +104,10 @@ class SproutImport extends Plugin
                     'label' => Craft::t('sprout-import', 'Weed'),
                     'url' => 'sprout-import/weed'
                 ],
-                'bundles' => [
-                    'label' => Craft::t('sprout-import', 'Bundles'),
-                    'url' => 'sprout-import/bundles'
-                ],
+//                'bundles' => [
+//                    'label' => Craft::t('sprout-import', 'Bundles'),
+//                    'url' => 'sprout-import/bundles'
+//                ],
                 'settings' => [
                     'label' => Craft::t('sprout-import', 'Settings'),
                     'url' => 'sprout-import/settings/general'
